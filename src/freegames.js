c59d2d0243d102fff5aa0b34bdfd81fd6e97efdc
Lucas Martin de Macedo Gagliano <31822176+lucasmmg1@users.noreply.github.com>
2025-06-04 01:46:26 -0300

    Schedule freegames command at 11am

--- src/freegames.js
+++ src/freegames.js
@@ -8,7 +8,7 @@ export default {
     name: "dailyFreeGames",
     once: true,
     execute(client) {
-        cron.schedule('45 23 * * *', async () => {
+        cron.schedule('0 11 * * *', async () => {
             const channel = await client.channels.fetch(CHANNEL_ID);
             if (!channel) return;
 
