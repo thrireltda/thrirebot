3cd85344f28d43936277e5bf5c057caf15c791ec
Lucas Martin de Macedo Gagliano <lucasmartinmacedo@gmail.com>
2025-08-21 20:42:54 -0300

    adição de uma frase de introdução

--- src/facades/voiceConnection.js
+++ src/facades/voiceConnection.js
@@ -1,4 +1,6 @@
 import { createAudioPlayer, getVoiceConnection, joinVoiceChannel } from "@discordjs/voice";
+import speakAndPlay from "../services/speakAndPlay.js";
+import random from "../commands/thrirebot/radio/random.js";
 
 export default class voiceConnection
 {
@@ -6,6 +8,25 @@ export default class voiceConnection
 
     static async join(interaction, client)
     {
+        const randomPhrase =
+        [
+            "Qual a boa de hoje?",
+            "Qual a boa?",
+            "O que manda?",
+            "E aí, firmeza?",
+            "Tudo certo?",
+            "Como vai essa força?",
+            "Beleza?",
+            "Qual o plano?",
+            "Tá de boas?",
+            "E as novidades?",
+            "Manda a real!",
+            "O que conta de novo?",
+            "Suave?",
+            "Qual a missão de hoje?",
+            "Bora pra cima?",
+        ];
+
         client.audioPlayer = await createAudioPlayer();
         client.audioPlayer.musicQueue = [];
         client.audioPlayer.isPlaying = false;
@@ -24,6 +45,16 @@ export default class voiceConnection
             client.emit('voiceConnectionAvailable', this.connection);
             resolve();
         })
+        .then(async () =>
+        {
+            const now = new Date();
+            const hours = now.getHours();
+            const greetings = hours >= 5 && hours < 12 ? "Bom dia" : hours >= 12 && hours < 18 ? "Boa tarde" : "Boa noite";
+            const displayName = interaction.user.globalName || interaction.user.username;
+            const firstName = displayName.split(" ")[0]; // pega só a primeira palavra
+            await speakAndPlay(client, `${greetings}, ${firstName}. ${randomPhrase[Math.floor(Math.random() * randomPhrase.length)]}`);
+        })
+        .catch(console.error);
     }
     static async leave(state)
     {
